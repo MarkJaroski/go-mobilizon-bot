bf597fe147e878361cead8467e39a5072bab2aac
Mark Andrew Jaroski <mobilizon@rationa.li>
2026-03-21 14:10:30 +0100

    decided on an architecture for the mobilizon client

--- mobilizon/client.go
+++ mobilizon/client.go
@@ -1,36 +1,208 @@
+// Package mobilizon implements a Mobilizon GraphQL client for golang
 package mobilizon
 
 import (
-	"net/http"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
 
+	"github.com/Khan/genqlient/graphql"
+	"github.com/google/uuid"
 	"github.com/hashicorp/go-hclog"
-	"github.com/hasura/go-graphql-client"
+	"golang.org/x/oauth2"
 )
 
-// Client represents a Mobilizon API client
+// Client wraps the genqlient GraphQL client
 type Client struct {
-	baseURL    string
-	gqlClient  *graphql.Client
-	httpClient *http.Client
-	auth       *AuthConfig
-	logger     hclog.Logger
+	baseURL      string
+	clientID     string
+	log          hclog.Logger
+	oauth2Config *oauth2.Config
+	token        *oauth2.Token
+	gqlClient    graphql.Client
 }
 
-// Config holds client configuration
-type Config struct {
-	BaseURL        string
-	Logger         hclog.Logger
-	HTTPClient     *http.Client
-	AuthConfigPath string
+// AuthConfig holds OAuth2 tokens
+type AuthConfig struct {
+	AccessToken  string    `json:"access_token"`
+	RefreshToken string    `json:"refresh_token"`
+	TokenType    string    `json:"token_type"`
+	Expiry       time.Time `json:"expiry"`
 }
 
 // NewClient creates a new Mobilizon client
-func NewClient(config Config) (*Client, error) {
-	// Implementation
+func NewClient(baseURL string, clientID string, log hclog.Logger) *Client {
+	return &Client{
+		baseURL:  baseURL,
+		log:      log,
+		clientID: clientID,
+		oauth2Config: &oauth2.Config{
+			ClientID: clientID,
+			Scopes: []string{
+				"write:event:create",
+				"write:event:update",
+				"write:media:upload",
+			},
+			Endpoint: oauth2.Endpoint{
+				AuthURL:       baseURL + "/oauth/authorize",
+				TokenURL:      baseURL + "/oauth/token",
+				DeviceAuthURL: baseURL + "/login/device/code",
+			},
+		},
+	}
 }
 
-// WithAuth sets authentication for the client
-func (c *Client) WithAuth(auth *AuthConfig) *Client {
-	c.auth = auth
-	return c
+func (c *Client) Authorize(ctx context.Context) error {
+	// get the device code
+	deviceAuth, err := c.oauth2Config.DeviceAuth(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get device code: %w", err)
+	}
+
+	// Display instructions to user
+	fmt.Printf("\n=== Device Authorization ===\n")
+	fmt.Printf("Visit: %s\n", deviceAuth.VerificationURI)
+	fmt.Printf("Enter code: %s\n", deviceAuth.UserCode)
+	fmt.Printf("============================\n\n")
+
+	// Wait for user to authorize (or use automatic polling)
+	fmt.Println("Waiting for authorization...")
+
+	// Poll for access token
+	token, err := c.oauth2Config.DeviceAccessToken(ctx, deviceAuth)
+	if err != nil {
+		return fmt.Errorf("failed to get access token: %w", err)
+	}
+
+	c.token = token
+
+	httpClient := c.oauth2Config.Client(ctx, c.token)
+	c.gqlClient = graphql.NewClient(c.baseURL+"/api", httpClient)
+
+	return nil
+}
+
+func (c *Client) Register() error {
+	return nil
+}
+
+// SaveToken saves the token to a file
+func (c *Client) SaveToken(filepath string) error {
+	if c.token == nil {
+		return fmt.Errorf("no token to save")
+	}
+
+	data, err := json.MarshalIndent(c.token, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filepath, data, 0600)
+}
+
+// LoadToken loads a token from a file
+func (c *Client) LoadToken(filepath string) error {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+
+	var token oauth2.Token
+	if err := json.Unmarshal(data, &token); err != nil {
+		return err
+	}
+
+	c.token = &token
+	return nil
+}
+
+// UploadMediaFile uploads a file and returns the media UUID
+func (c *Client) UploadMediaFile(ctx context.Context, filepath string) (*uuid.UUID, error) {
+	// Open the file
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Use generated UploadMedia function
+	resp, err := UploadMedia(ctx, c.gqlClient,
+		graphql.Upload{
+			FileName: filepath,
+			Body:     file,
+		},
+		filepath,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp.UploadMedia.Uuid, nil
+}
+
+// CreateEventWithMedia creates an event with an image
+func (c *Client) CreateEventWithMedia(
+	ctx context.Context,
+	params CreateEventParams,
+	imagePath string,
+) (*uuid.UUID, error) {
+	// First upload the media
+	mediaUUID, err := c.UploadMediaFile(ctx, imagePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to upload media: %w", err)
+	}
+
+	// Create the event with the media UUID
+	resp, err := createEvent(
+		ctx,
+		c.gqlClient,
+		strconv.Itoa(params.OrganizerActorId),
+		strconv.Itoa(params.AttributedToId),
+		params.Title,
+		params.Description,
+		params.BeginsOn,
+		params.EndsOn,
+		params.Status,
+		params.Visibility,
+		params.JoinOptions,
+		params.OnlineAddress,
+		params.Draft,
+		params.Tags,
+		MediaInput{MediaUuid: *mediaUUID}, // Use uploaded media
+		params.OnlineAddress,
+		params.Category,
+		params.PhysicalAddress,
+		params.Options,
+		params.Contact,
+		params.Metadata,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp.CreateEvent.Uuid, nil
+}
+
+// SearchForEvents searches for events by term
+func (c *Client) SearchForEvents(ctx context.Context, term string, beginsOn time.Time) ([]Event, error) {
+	resp, err := SearchEvents(ctx, c.gqlClient, term, beginsOn)
+	if err != nil {
+		return nil, err
+	}
+
+	events := make([]Event, len(resp.SearchEvents.Elements))
+	for i, elem := range resp.SearchEvents.Elements {
+		events[i] = Event{
+			ID:            elem.Id,
+			UUID:          elem.Uuid,
+			Title:         elem.Title,
+			BeginsOn:      elem.BeginsOn,
+			OnlineAddress: elem.OnlineAddress,
+		}
+	}
+
+	return events, nil
 }
---
+++ mobilizon/client_test.go
@@ -0,0 +1,50 @@
+// mobilizon/client_test.go
+package mobilizon
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Khan/genqlient/graphql"
+)
+
+// MockGraphQLClient for testing
+type MockGraphQLClient struct {
+	MakeRequestFunc func(
+		ctx context.Context,
+		req *graphql.Request,
+		resp *graphql.Response,
+	) error
+}
+
+func (m *MockGraphQLClient) MakeRequest(
+	ctx context.Context,
+	req *graphql.Request,
+	resp *graphql.Response,
+) error {
+	if m.MakeRequestFunc != nil {
+		return m.MakeRequestFunc(ctx, req, resp)
+	}
+	return nil
+}
+
+func TestClient_CreateEvent(t *testing.T) {
+	mock := &MockGraphQLClient{
+		MakeRequestFunc: func(ctx context.Context, req *graphql.Request, resp *graphql.Response) error {
+			// Simulate successful response
+			resp.Data = map[string]interface{}{
+				"createEvent": map[string]interface{}{
+					"id":    "123",
+					"uuid":  "test-uuid",
+					"title": "Test Event",
+				},
+			}
+			return nil
+		},
+	}
+
+	client := &Client{gqlClient: mock}
+
+	// Test using generated function
+	// ...
+}
